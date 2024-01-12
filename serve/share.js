a4c78b1293d6ff292dd0839ebbb9877f57685890
chillymind <do@visioninmind.net>
2024-01-12 22:37:48 +0900

    kakaoapi sharing

---
+++ serve/share.js
@@ -0,0 +1,32 @@
+
+const kakaoApiInitialize = () => {
+    Kakao.init('f07d0163ad5f23561d55e2cb59ee286a'); 
+    Kakao.Share.createDefaultButton({
+        container: '#kakaotalk-sharing-btn',
+        objectType: 'feed',
+        content: {
+        title: '이정욱 정지현 - 4월 28일 일요일 11:30 빌라드 지디 강남',
+        description: '#초대합니다#하우스웨딩',
+        imageUrl:
+            'https://jimyaooya.github.io/joisgettingmarried/imgs/wp000a.jpg',
+        link: {
+            mobileWebUrl: 'https://jimyaooya.github.io/joisgettingmarried',
+            webUrl: 'https://jimyaooya.github.io/joisgettingmarried',
+        },
+        },
+        social: {
+        likeCount: 286,
+        commentCount: 45,
+        sharedCount: 845,
+        },
+        buttons: [
+        {
+            title: '보러가기',
+            link: {
+            mobileWebUrl: 'https://jimyaooya.github.io/joisgettingmarried',
+            webUrl: 'https://jimyaooya.github.io/joisgettingmarried',
+            },
+        },
+        ],
+    });
+}
