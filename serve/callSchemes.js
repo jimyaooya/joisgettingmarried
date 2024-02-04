80f82488faac10edae57e5513e07eb3c5d0e5e66
chillymind <do@visioninmind.net>
2024-02-04 19:25:53 +0900

    navi dim 추가

--- serve/callSchemes.js
+++ serve/callSchemes.js
@@ -1,3 +1,40 @@
+let currentTargertScheme = undefined;
+
+const setScheme = (scheme) => {
+    currentTargertScheme = scheme;
+
+    // get dim element
+    const dim = document.getElementById('dim_for_navi');
+    dim.style.display = 'visible';
+}
+
+const closeNaviDim = () =>{
+    const dim = document.getElementById('dim_for_navi');
+    dim.style.display = 'none';
+}
+
+const toParkingLot = () => {
+    try{
+        const lat = 37.51675036659504;
+        const lng = 127.04100014039027;
+        const name = 'SK 허브블루 주차장';
+        callSchemes(lat, lng, name);
+    }finally{
+        closeNaviDim();
+    }
+}
+
+const toWeddingHall = () => {
+    try{
+        const lat = 37.5140508;
+        const lng = 127.0372835;
+        const name = '빌라드지디강남';
+        callSchemes(lat, lng, name);
+    }finally{
+        closeNaviDim();
+    }
+}
+
 const callTmap = (lat, lng, name) => {
     const url = `tmap://route?goalx=${lng}&goaly=${lat}&goalname=${name}&appScheme=callSchemes://`;
     window.location.href = url;
@@ -11,6 +48,18 @@ const callKakaomap = (lat, lng) => {
 const callNavermap = (lat, lng, name) => {
     const url = `nmap://route/walk?dlat=${lat}&dlng=${lng}&dname=${name}&appScheme=callSchemes://`;
     window.location.href = url;
+}
+
+const callSchemes = (lat, lng, name) => {
+    if(currentTargertScheme === 'tmap') {
+        callTmap(lat, lng, name);
+    } else if(currentTargertScheme === 'kakaomap') {    
+        callKakaomap(lat, lng);
+    } else if(currentTargertScheme === 'navermap') {
+        callNavermap(lat, lng, name);
+    }else{
+        console.error('Invalid scheme');
+    }
 }
 
 const copyToClipboard = (text) => {
